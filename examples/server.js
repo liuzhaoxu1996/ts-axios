178d531bbbfeab1fd66f8de68ad5d2c2a29c3aed
liuzhaoxu <i-liuzhaoxu@360.cn>
2020-06-28 19:16:07 +0800

    feat:  处理请求body数据

--- examples/server.js
+++ examples/server.js
@@ -8,8 +8,6 @@ const WebpackConfig = require('./webpack.config')
 const app = express()
 const compiler = webpack(WebpackConfig)
 
-
-
 app.use(webpackDevMiddleware(compiler, {
   publicPath: '/__build__/',
   stats: {
@@ -30,6 +28,27 @@ const router = express.Router()
 router.get('/simple/get', function(req, res) {
   res.json({
     msg: `hello world`
+  })
+})
+
+router.get('/base/get', function(req, res) {
+  res.json(req.query)
+})
+
+router.post('/base/post', function(req, res) {
+  res.json(req.body)
+})
+
+router.post('/base/buffer', function(req, res) {
+  let msg = []
+  req.on('data', (chunk) => {
+    if (chunk) {
+      msg.push(chunk)
+    }
+  })
+  req.on('end', () => {
+    let buf = Buffer.concat(msg)
+    res.json(buf.toJSON())
   })
 })
 
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,24 @@
 import { AxiosRequestConfig } from './types'
 import xhr from './xhr'
+import { bulidURL } from './helpers/url'
+import { transformRequest } from './helpers/data'
+
 function axios(config: AxiosRequestConfig): void {
+  processConfig(config)
   xhr(config)
 }
 
+function processConfig (config: AxiosRequestConfig): void {
+  config.url = transformUrl(config)
+  config.data = transformRequestData(config)
+}
+
+function transformUrl (config: AxiosRequestConfig): string {
+  const { url, params } = config
+  return bulidURL(url, params)
+}
+
+function transformRequestData (config: AxiosRequestConfig): any {
+  return transformRequest(config.data)
+}
 export default axios
